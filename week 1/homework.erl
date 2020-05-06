dae1cf24c157bbfd051ca1120b7dfd99bfc1bc1c
Matt Willy <matt.willy@gmail.com>
2020-05-06 10:14:54 -0600

    set of solutions for the homework for the erlang course.

---
+++ week 1/homework.erl
@@ -0,0 +1,26 @@
+-module(homework).
+-export([xOr1/2, xOr2/2,xOr3/2, maxThree/3,howManyEqual/3]).
+
+% three ways to represent xOr.
+xOr1(X, Y) ->
+    not X == Y. 
+
+xOr2(X, Y) ->
+    X =/= Y.
+
+xOr3(X, Y) ->
+    (X and not Y) or (not X and Y).
+
+maxThree(X, Y, Z) ->
+    max(max(X, Y), Z).
+
+howManyEqual(X, X, X) ->
+    3;
+howManyEqual(X, X, _) ->
+    2;
+howManyEqual(X, _, X) ->
+    2;
+howManyEqual(_, X, X) ->
+    2;
+howManyEqual(_, _, _) ->
+    0.
